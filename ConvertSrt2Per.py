06540cb8cb93b46ba327fb29398e3b1b29316e3d
Kia Hamedi <kia.arta9793@gmail.com>
2020-04-01 15:08:22 +0430

    added show informations

--- ConvertSrt2Per.py
+++ ConvertSrt2Per.py
@@ -4,6 +4,8 @@
 #kia.arta9793@gmail.com
 
 import sys
+from sys import platform
+from os import system
 from importlib import reload
 from googletrans import Translator
 
@@ -15,12 +17,22 @@ translator = Translator()
 input_file_name = sys.argv[1]
 output_file_name = sys.argv[2]
 
-f_out = open(output_file_name, 'w')
+number_of_lines = 0
+translated_lines = 0
 
+print('--------------------------------------------------')
+
+if platform == "linux" or platform == "linux2":
+    print("Total Lines:")
+    system("cat {} | wc -l ".format(input_file_name.replace(' ','\ ')))
+
+f_out = open(output_file_name, 'w')
 with open(input_file_name) as f_in:
     lines = f_in.readlines()
 
     for line in lines:
+        number_of_lines += 1
+        print("> Line of: " + str(number_of_lines))
         if line.isdigit() and len(line) < 4:
             f_out.write(line)
             continue
@@ -39,9 +51,10 @@ with open(input_file_name) as f_in:
 
         translated = translator.translate(line, src='en', dest='fa')
         f_out.write(translated.text + "\r\n")
+        translated_lines += 1
 
 f_in.close()
 f_out.close()
-
-
-
+print('--------------------------------------------------')
+print("Total number of lines is:", str(number_of_lines))
+print("Total number of translated lines is:", str(translated_lines))
